8dafd7c0bcfb73c316c7e0eb93c25bfc522a2c51
Abraham2908 <abrahampedrog@gmail.com>
2023-10-06 12:11:16 -0300

    Ajustando recon

--- main.py
+++ main.py
@@ -40,16 +40,15 @@ async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_teleg
             await bot.send_photo(chat_id=chat_id, photo=photo)
         await bot.send_message(chat_id=chat_id, text="Welcome to Cyclic Starter Python Telegram Bot!")
 
-    elif text == "/recon":
-     
-        if text.startswith("/recon"):
-            parts = text.split()
-            if len(parts) >= 2:
-                domain = parts[1]
-                await bot.send_message(chat_id=chat_id, text=f"Domínio: {domain}")
-            else:
-                await bot.send_message(chat_id=chat_id, text="Por favor informe um domínio depois do comando. Ex: /recon <domínio>")
-                return
+    elif text.startswith("/recon"):
+      
+        parts = text.split()
+        if len(parts) >= 2:
+            domain = parts[1]
+            await bot.send_message(chat_id=chat_id, text=f"Domínio: {domain}")
+        else:
+            await bot.send_message(chat_id=chat_id, text="Por favor informe um domínio depois do comando. Ex: /recon <domínio>")
+            return
         
         menu_keyboard = [[KeyboardButton(text="Subfinder")], [KeyboardButton(text="Cancelar")]]  
         markup = ReplyKeyboardMarkup(menu_keyboard)
